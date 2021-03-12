aa6cf54a0eb104faa5aa71c168be330fcc771c31
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2021-03-12 19:43:09 +0300

    feat: add js code

--- script.js
+++ script.js
@@ -23,117 +23,130 @@
 // } else  alert(‘Пароль не верный’)
 
 
-let regInfo = [];
+let regInfo = []; 
+ 
+const sections1 = document.querySelector('.log-page'); 
+const sections2 = document.querySelector('.get-page'); 
+const sections3 = document.querySelector('.welcome-page'); 
+ 
+const inputEmail = document.querySelector('#get_input_email'); 
+const inputPass = document.querySelector('#get_input_pass'); 
+const inputPassRepeat = document.querySelector('#get_input_repeat_pass'); 
+ 
+const inputLogin = document.querySelector('#welcome_input_email'); 
+const inputPassword = document.querySelector('#welcome_input_pass'); 
+ 
+const regBtn = document.querySelector('#reg-btn'); 
+ 
+function MainConstructor() { 
+    this.doesUserExisted = false; 
+    this.showMainPage = function() { 
+        sections1.style.display = 'flex' 
+        sections2.style.display = 'none' 
+        sections3.style.display = 'none' 
+    }; 
+    this.showRegistrationPage = function() { 
+        sections1.style.display = 'none' 
+        sections2.style.display = 'flex' 
+        sections3.style.display = 'none' 
+    }; 
+    this.showAuthPage = function() { 
+        sections1.style.display = 'none' 
+        sections2.style.display = 'none' 
+        sections3.style.display = 'flex' 
+    }; 
+    this.saveReg = function () { 
+        const save = {email: inputEmail.value, password: inputPass.value, repeatPassword: inputPassRepeat.value}; 
+        if(!this.doesUserExisted){ 
+            regInfo.push(save); 
+        } 
+    }; 
+    this.mailValid = function () { 
+        this.doesUserExisted = regInfo.find(user => { 
+            return user.email === inputLogin.value 
+          })
+        this.doesUserExisted2 = regInfo.find(user => { 
+            return user.password === inputPassword.value 
+          })   
+          if(!this.doesUserExisted && !this.doesUserExisted2) { 
+            alert("Вы вошли в систему") 
+            this.doesUserExisted = false;
+            this.doesUserExisted2 = false; 
+          }else {alert("Юзер такой есть")
+        } 
+    }; 
+};
 
-const sections1 = document.querySelector('.log-page');
-const sections2 = document.querySelector('.get-page');
-const sections3 = document.querySelector('.welcome-page');
 
-const inputEmail = document.querySelector('#get_input_email');
-const inputPass = document.querySelector('#get_input_pass');
-const inputPassRepeat = document.querySelector('#get_input_repeat_pass');
-
-const regBtn = document.querySelector('#reg-btn');
-
-// const handleRegistration = () => {
-//     regInfo.push(inputEmail.value);
-//     regInfo.push(inputPass.value);
-//     regInfo.push(inputPassRepeat.value);
-// };
-
-
-function MainConstructor() {
-    this.showMainPage = function() {
-        sections1.style.display = 'flex'
-        sections2.style.display = 'none'
-        sections3.style.display = 'none'
-    };
-    this.showRegistrationPage = function() {
-        sections1.style.display = 'none'
-        sections2.style.display = 'flex'
-        sections3.style.display = 'none'
-    };
-    this.showAuthPage = function() {
-        sections1.style.display = 'none'
-        sections2.style.display = 'none'
-        sections3.style.display = 'flex'
-    };
-    this.saveReg = function () {
-        regInfo.push(inputEmail.value);
-        regInfo.push(inputPass.value);
-        regInfo.push(inputPassRepeat.value);
-    };
-}
-
-console.log(regInfo);
 
 
 const newObj = new MainConstructor();
-
+newObj.showMainPage();
 
 
 // OTHER
 
 
 
-const buttonSignUp = document.querySelector('.button-sign-up')
-const buttonLogIn = document.querySelector('.button-log-in')
-const buttonMainPage = document.querySelectorAll('.button-main-page')
-const logIn = document.querySelector('.log-in')
-const signUp = document.querySelector('.sign-up')
-const mainPage = document.querySelector('.main-page')
 
-const adressRegistration = document.querySelector('.adress-registration')
-const passwordRegistration = document.querySelector('.password-registration')
-const passwordRegistrationRepeat = document.querySelector('.password-registration-repeat')
-const adressLogIn = document.querySelector('.adress-log-in')
-const passwordLogIn = document.querySelector('.password-log-in')
+// const buttonSignUp = document.querySelector('.button-sign-up')
+// const buttonLogIn = document.querySelector('.button-log-in')
+// const buttonMainPage = document.querySelectorAll('.button-main-page')
+// const logIn = document.querySelector('.log-in')
+// const signUp = document.querySelector('.sign-up')
+// const mainPage = document.querySelector('.main-page')
 
-const sendRegistration = document.querySelector('.send-registration')
-const sendLogIn = document.querySelector('.send-log-in')
+// const adressRegistration = document.querySelector('.adress-registration')
+// const passwordRegistration = document.querySelector('.password-registration')
+// const passwordRegistrationRepeat = document.querySelector('.password-registration-repeat')
+// const adressLogIn = document.querySelector('.adress-log-in')
+// const passwordLogIn = document.querySelector('.password-log-in')
 
-let arrFormRegistration = []
-const Site = function() {
-    this.showMainPage = function() {
-        mainPage.style.display = 'block'
-        signUp.style.display = 'none'
-        logIn.style.display = 'none'
-        const self=this
-        buttonLogIn.addEventListener('click', self.showAuthPage)
-        buttonSignUp.addEventListener('click', self.showRegistrationPage)
-    }
-    this.showRegistrationPage = function() {
-        mainPage.style.display = 'none'
-        signUp.style.display = 'block'
-        logIn.style.display = 'none'
-        const self=this
-        buttonMainPage[1].addEventListener('click', self.showMainPage)
-        sendRegistration.addEventListener('click', self.registration)
-        // adressRegistration.value = ''
-        //     passwordRegistration.value = ''
-        //     passwordRegistrationRepeat.value = ''
-        buttonMainPage[1].removeEventListener('click', self.registration)
-    }
-    this.showAuthPage = function() {
-        mainPage.style.display = 'none'
-        signUp.style.display = 'none'
-        logIn.style.display = 'block'
-        const self=this
-        buttonMainPage[0].addEventListener('click', self.showMainPage)
-        sendLogIn.addEventListener('click', self.login)
+// const sendRegistration = document.querySelector('.send-registration')
+// const sendLogIn = document.querySelector('.send-log-in')
 
-    }
-    this.registration = function() {
-        const user = {email: adressRegistration.value, password: passwordRegistration.value, repeatPassword: passwordRegistrationRepeat.value}
-        arrFormRegistration.push(user)
-        console.log(arrFormRegistration);
-    }
-    this.login = function() {
-        const user = arrFormRegistration.find(currentUser => currentUser.email === adressLogIn.value)
-        if (user.password = passwordLogIn.value) alert('you are log in')
-    }
+// let arrFormRegistration = []
+// const Site = function() {
+//     this.showMainPage = function() {
+//         mainPage.style.display = 'block'
+//         signUp.style.display = 'none'
+//         logIn.style.display = 'none'
+//         const self=this
+//         buttonLogIn.addEventListener('click', self.showAuthPage)
+//         buttonSignUp.addEventListener('click', self.showRegistrationPage)
+//     }
+//     this.showRegistrationPage = function() {
+//         mainPage.style.display = 'none'
+//         signUp.style.display = 'block'
+//         logIn.style.display = 'none'
+//         const self=this
+//         buttonMainPage[1].addEventListener('click', self.showMainPage)
+//         sendRegistration.addEventListener('click', self.registration)
+//         // adressRegistration.value = ''
+//         //     passwordRegistration.value = ''
+//         //     passwordRegistrationRepeat.value = ''
+//         buttonMainPage[1].removeEventListener('click', self.registration)
+//     }
+//     this.showAuthPage = function() {
+//         mainPage.style.display = 'none'
+//         signUp.style.display = 'none'
+//         logIn.style.display = 'block'
+//         const self=this
+//         buttonMainPage[0].addEventListener('click', self.showMainPage)
+//         sendLogIn.addEventListener('click', self.login)
 
-}
+//     }
+//     this.registration = function() {
+//         const user = {email: adressRegistration.value, password: passwordRegistration.value, repeatPassword: passwordRegistrationRepeat.value}
+//         arrFormRegistration.push(user)
+//         console.log(arrFormRegistration);
+//     }
+//     this.login = function() {
+//         const user = arrFormRegistration.find(currentUser => currentUser.email === adressLogIn.value)
+//         if (user.password = passwordLogIn.value) alert('you are log in')
+//     }
 
-const site = new Site()
-site.showMainPage()
+// }
+
+// const site = new Site()
+// site.showMainPage()
